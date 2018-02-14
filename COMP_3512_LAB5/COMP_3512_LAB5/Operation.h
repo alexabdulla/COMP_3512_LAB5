bd5d20a983854974f9debe6c8ba3218e039a197f
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-02-13 20:46:28 -0800

    Checking that its synched properly

---
+++ COMP_3512_LAB5/COMP_3512_LAB5/Operation.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Operation {
+	public: 
+		// purely virtual?
+		char get_code();
+
+		// purely virtual?
+		int perform(int, int);
+		
+		// inline implemetation?
+		~Operation();
+
+		//testing
+
+};
+
