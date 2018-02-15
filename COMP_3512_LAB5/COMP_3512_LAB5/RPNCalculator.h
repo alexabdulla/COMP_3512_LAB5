9155391b27f5d5d35bda8fd7a31f01454090d722
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-02-15 08:14:23 -0800

    Wow this is getting hard. Combined with all of our other stuff this is a pretty crazy lab to give us before midterms chris!!
    Added ore fx

--- COMP_3512_LAB5/COMP_3512_LAB5/RPNCalculator.h
+++ COMP_3512_LAB5/COMP_3512_LAB5/RPNCalculator.h
@@ -2,19 +2,48 @@
 #include <stack>;
 #include "Operation.h"
 #include "AdditionOperation.h"
+#include "SubtractionOperation.h"
+#include "MultiplicationOperation.h"
+#include "DivisionOperation.h"
 
 class RPNCalculator {
 	private:
 		int result;
 		std::stack<int> stack;
 
-		// this seems to work, but not with operation...hmm...
-		Operation* operation_type(int operation) {
-			Operation* temp;
-			AdditionOperation* additionOp = new AdditionOperation(operation);
-			//temp = additionOp;
+		// this seems to work, but not with operation...hmm...just abtractOp?
+		// we need to do this with public constants ? and with opperaion*
+		AbstractOperation* operation_type(int operation) {
+			AbstractOperation* temp;
+
+			switch (operation) {
+			
+			case '+' :
+				temp = new AdditionOperation();
+			case '-' :
+				temp = new SubtractionOperation();
+			case '/':
+				temp = new DivisionOperation();
+			case '*':
+				temp = new MultiplicationOperation();
+			}
+			
+			
 			return temp;
 		};
+
+		void perform(Operation*) {
+			// pop two numbers from the stack
+			// apply the op to those numbers
+			// push the results back on the stack
+		}
+
+		int process_form(std::string formula) {
+			//reads formula from left to right
+			// push ints from formula to the stack 
+			// bunch of wierd instructions i dont understand
+			// yeah more wierd stuff, we are fucked
+		}
 
 
 };
